b2aa167b0bf6bb053f005931c090e7061e4a7100
Gregor Klevze <gregor@klevze.si>
2020-02-17 12:31:08 +0100

    fix

--- js/textSlider.js
+++ js/textSlider.js
@@ -17,7 +17,7 @@
 	this.textSlider = function(obj) {
 		objName = obj;
 		this.init();
-	}
+	};
 
 	this.init = function() {
 		this.currentPage = 0;
@@ -25,7 +25,7 @@
 		this.numPages = this.getPages();
 		this.showPage();
 		this.bindEvents();
-	}
+	};
 
 	this.calc = function() {
 		this.w = this.getWidth();
@@ -35,33 +35,33 @@
 		this.padding = $(objName).css("padding");
 		this.cw = Math.round(this.w / this.cols + this.gap / this.cols + 0.99);
 		this.mainWidth = this.getFullWidth();
-	}
+	};
 
 	this.getPages = function() {
 		//console.log(this.mainWidth, this.cw);
 		return Math.round(this.mainWidth / this.cw) - 1;
-	}
+	};
 
 	this.getFullWidth = function() {
 		return  Math.round($(objName).find(":last").position().left);
-	}
+	};
 
 	this.getWidth = function() {
 		return $(objName).width();
-	}
+	};
 
 	this.getGap = function() {
 		return parseInt($(objName).css("column-gap"), 10);
-	}
+	};
 
 	this.getCols = function() {
 		return parseInt($(objName).css("column-count"), 10);
-	}
+	};
 
 	this.showPage = function() {
 		this.displayPage = this.currentPage + 1;
 		$(".page").html( this.displayPage + "/" + this.numPages);
-	}
+	};
 
 	this.bindEvents = function () {
 		document.querySelector(".slide-left").addEventListener("click", (e) => { 
